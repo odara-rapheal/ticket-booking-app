be2f06dbeccfc7938b3f9ea9b187915c3d18864c
odara-rapheal <odararapheal@gmail.com>
2025-06-09 19:02:41 +0100

    chages made to codebase

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ func main() {
 	var appName = "I-DRA"
 	fmt.Println("Welcome to", appName, "ticket booking application")
 	fmt.Println("How can", appName, "help you?")
-	//fmt.Printf("This is the official ticket booking app made by %v\n", appName)
+	fmt.Printf("This is the official ticket booking app made by %v\n", appName)
 }
