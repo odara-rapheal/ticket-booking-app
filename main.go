06fa557148cc0daaf784cd51aa46e66100e5bf0e
odara-rapheal <odararapheal@gmail.com>
2025-06-09 18:57:38 +0100

    update

--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	var appName = "I-DRA"
 	fmt.Println("Welcome to", appName, "ticket booking application")
+	fmt.Printf("This is the official ticket booking app made by %v\n", appName)
 }
