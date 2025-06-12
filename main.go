60b7a012297ea46312ae0e05dc6fb1b9f3541942
odara-rapheal <odararapheal@gmail.com>
2025-06-12 20:45:02 +0100

    bug fix

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	var appName = "I-DRA"
 	fmt.Println("Welcome to", appName, "ticket booking application")
-	//fmt.Println("How can", appName, "help you?")
+	fmt.Println("How can", appName, "help you?")
 	fmt.Printf("This is the official ticket booking app made by %v\n", appName)
 }
